796dc68f7a1a69a1d44cfb587b1623f1f745333f
bwapes <benwaples@gmail.com>
2020-10-20 15:31:01 -0700

    detailed character displayed

--- src/components/DetailPage.jsx
+++ src/components/DetailPage.jsx
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 
 export default class DetailPage extends Component {
 
+  state = {
+    details: {}
+  }
+
+  componentDidMount = () => {
+    // eslint-disable-next-line max-len
+    fetch(`https://last-airbender-api.herokuapp.com/api/v1/characters/${this.props.match.params.characterId}`)
+      .then(res => res.json())
+      .then(details => this.setState({ details }));
+  }
+
   render() {
-    const characterId = this.props.match.params.characterId;
+    const { details } = this.state;
     return (
       <div>
-        {characterId}
+        {details.name}
       </div>
     );
   }
