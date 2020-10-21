fd570898406b27f10dd24bc9e221003d3b49f750
bwapes <benwaples@gmail.com>
2020-10-20 19:56:14 -0700

    styles

--- src/components/DetailPage.jsx
+++ src/components/DetailPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React, { Component } from 'react';
 import { fetchCharacter } from '../services/fetch-calls';
 import styles from '../App.css';
@@ -20,11 +21,11 @@ export default class DetailPage extends Component {
       <div className={styles.detailedCharacter}>
         <h1>{details.name}</h1>
         <img src={details?.photoUrl} alt={details.name}/>
-        <p>Identified Gender: {details?.gender}</p>
-        <p>Eye Color: {details?.eye}</p>
-        <p>Love: {details?.love}</p>
-        <p>Weapon: {details?.weapon}</p>
-        <p>Profession: {details?.profession}</p>
+        <p>Identified Gender: {details.gender ? details.gender : 'No identified gender'}</p>
+        <p>Eye Color: {details.eye ? details.eye : 'No associated eye color'}</p>
+        <p>Love: {details.love ? details.love : 'No associated love'}</p>
+        <p>Weapon: {details.weapon ? details.weapon : 'No associated weapon'}</p>
+        <p>Profession: {details.profession ? details.profession : 'No associated profession'}</p>
         <p>Predecessor: {details?.predecessor}</p>
       </div>
     );
